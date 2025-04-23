907d8f23af495f459dc0e7b97604555e6f80a3ae
manoelsmoraes <63382033+manoelsmoraes@users.noreply.github.com>
2025-04-23 13:05:58 -0300

    Update contador.v
    Ok 2

--- contador.v
+++ contador.v
@@ -10,4 +10,22 @@ module Contador (
 
 // Insira seu código aqui
 
+
+// Valor inicial do contador após reset
+  initial begin
+      saida = 8'h6A; // Valor 106 em hexadecimal (0x6A)
+  end
+
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            saida <= 8'h6A; // Reset do contador para 106 (0x6A)
+        end else if (acrescer && !decrecer) begin
+            saida <= saida + 1; // Incrementa em 1
+        end else if (!acrescer && decrecer) begin
+            saida <= saida - 1; // Decrementa em 1
+        end
+        // Se ambos os sinais forem inativos ou ativos, o valor do contador não muda
+    end
+
+
 endmodule
